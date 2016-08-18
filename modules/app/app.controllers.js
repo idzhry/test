1cc54897a14ea8b50b4f106268d835c8160a77c9
gaofei <gaofei@bis.com.cn>
2016-08-18 11:06:28 +0800

    no message

--- modules/app/app.controllers.js
+++ modules/app/app.controllers.js
@@ -3,24 +3,24 @@ angular.module('starter.controllers', [])
   .controller('HomeCtrl', function ($scope, ionicDatePicker) {
 
     $scope.selectedDate1;
-    $scope.selectedDate2;
+    $scope.selectedDate2 = new Date();
 
-    $scope.openDatePickerOne = function (val) {
-      var ipObj1 = {
-        callback: function (val) {  //Mandatory
-          console.log('Return value from the datepicker popup is : ' + val, new Date(val));
-          $scope.selectedDate1 = new Date(val);
-        },
-        from: new Date(2012, 1, 1),
-        to: new Date(2016, 10, 30),
-        inputDate: new Date(),
-        mondayFirst: true,
-        disableWeekdays: [],
-        closeOnSelect: false,
-        templateType: 'popup'
-      };
-      ionicDatePicker.openDatePicker(ipObj1);
-    };
+    //$scope.openDatePickerOne = function (val) {
+    //  var ipObj1 = {
+    //    callback: function (val) {  //Mandatory
+    //      console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+    //      $scope.selectedDate1 = new Date(val);
+    //    },
+    //    from: new Date(2012, 1, 1),
+    //    to: new Date(2016, 10, 30),
+    //    inputDate: new Date(),
+    //    mondayFirst: true,
+    //    disableWeekdays: [],
+    //    closeOnSelect: false,
+    //    templateType: 'popup'
+    //  };
+    //  ionicDatePicker.openDatePicker(ipObj1);
+    //};
 
     $scope.openDatePickerTwo = function (val) {
       var ipObj1 = {
@@ -31,7 +31,7 @@ angular.module('starter.controllers', [])
         disabledDates: [
           new Date(2016, 8, 25)
         ],
-        inputDate: new Date(),
+        inputDate: $scope.selectedDate2,
         mondayFirst: true,
         showTodayButton: true,
         closeOnSelect: false,
